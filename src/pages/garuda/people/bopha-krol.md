a6a7f1037ba1e3fbeb550e32af4f78fe5b9754b1
Alex Nowicki <al_nowicki@hotmail.com>
2023-03-27 16:27:27 -0300

    Added initial css and working on layouts

--- src/pages/garuda/people/bopha-krol.md
+++ src/pages/garuda/people/bopha-krol.md
@@ -1,14 +1,17 @@
 ---
 layout: ../../../layouts/PeoplePostLayout.astro
 title: 'Bopha Krol'
-name: 'Bopha Krol'
-aliases: ["Bopha de la Rosa"]
-age: 38
-species: 'Human'
-gender: 'Male'
-profession: 'Captain of the Garuda' 
-motivations: ["Wants to be rich"]
-fears: ["Fears attachement and often sabotage his own relationships if they get too close."]
+metadata: 
+    name: 'Bopha Krol'
+    aliases: ["Bopha de la Rosa"]
+    age: 38
+    species: 'Human'
+    gender: 'Male'
+    profession: 'Captain of the Garuda'
 tags: ["people"]
 
 ---
+
+## Motivation
+
+He wants a dog.
---
+++ src/scripts/accordion.js
@@ -0,0 +1,42 @@
+(function () {
+
+    document.addEventListener('click', function(event){
+
+        let target = event.target;
+        
+        if (target.classList.contains('accordion-toggle')){
+
+        let accordion = event.target.closest('.accordion');
+
+        // Check if the current toggle is expanded.
+        let isExpanded = target.getAttribute('aria-expanded') == 'true';
+        let active = accordion.querySelector('[aria-expanded="true"]');
+
+        // Close the open accordion if not active
+        if (active && active !== target) {
+        // Reset the active toggle
+        active.setAttribute('aria-expanded', 'false');
+        // Hide the accordion panel, using aria-controls to specify the desired section
+        document.querySelector(`#${active.getAttribute('aria-controls')}`).setAttribute('hidden', '');
+        }
+
+        if (!isExpanded) {
+        // Set the expanded state on the triggering element
+        target.setAttribute('aria-expanded', 'true');
+        // Hide the accordion sections, using aria-controls to specify the desired section
+        document.querySelector(`#${target.getAttribute('aria-controls')}`).removeAttribute('hidden');
+        }
+
+        else if (isExpanded) {
+        // Set the expanded state on the triggering element
+        target.setAttribute('aria-expanded', 'false');
+        // Hide the accordion sections, using aria-controls to specify the desired section
+        document.querySelector(`#${target.getAttribute('aria-controls')}`).setAttribute('hidden', '');
+        }
+
+        event.preventDefault();
+        
+        }
+    })
+
+})
