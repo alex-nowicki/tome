5bbcdfb65f90146c9b3c09ba389bf69f83f29d7d
Alex Nowicki <al_nowicki@hotmail.com>
2023-04-27 15:29:05 -0300

    Added Related Articles to Post Layout

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -3,59 +3,75 @@
 //
 
 /**
- * Toggle accordion collapse state on click events
+ * Show content on click events
  * @param  {Event} event The event object
  */
-let bookmarksClickHandler = function(event) {
+let accordionClickHandler = function(event) {
 
+        // Only run on accordion buttons
+        if (!event.target.matches('.accordion-toggle')) return;
+
+        event.preventDefault();
+
+        // Toggle accordion visibility
+        toggleAccordion(event.target);
+
+}
+
+/**
+ * Toggle accordion visibility
+ * @param  {Node} targetAccordion The accordion to show
+ */
+let toggleAccordion = function(targetAccordion) {
+
+    // Get the accordion group
+    let accordionGroup = targetAccordion.closest('.accordion');
+    
+    // Check whether the accordion group allows multiple open panels
+    let allowMultiple = accordionGroup.hasAttribute('data-allow-multiple');
+
+    // Check if the current panel is expanded
+    let isExpanded = targetAccordion.getAttribute('aria-expanded') == 'true';
+    
+    // Get the active toggle
+    let active = accordionGroup.querySelector('[aria-expanded="true"]');
+
+    // If the target is not active, and allow multiple is false, close the open accordion if not active
+    if (!allowMultiple && active && active !== targetAccordion) {
+
+        // Reset the active toggle
+        active.setAttribute('aria-expanded', 'false');
+        
+        // Hide the accordion panel, using aria-controls to specify the desired section
+        document.querySelector(`#${active.getAttribute('aria-controls')}`).setAttribute('hidden', '');
+
+    }
+
+    // If the target is closed, open the accordion
+    if (!isExpanded) {
+
+        // Set the expanded state on the triggering element
+        targetAccordion.setAttribute('aria-expanded', 'true');
+
+        // Hide the accordion sections, using aria-controls to specify the desired section
+        document.querySelector(`#${targetAccordion.getAttribute('aria-controls')}`).removeAttribute('hidden');
+    }
+
+    // If the target is open, close the accordion
+    else if (isExpanded) {
+
+        // Set the expanded state on the triggering element
+        targetAccordion.setAttribute('aria-expanded', 'false');
+
+        // Hide the accordion sections, using aria-controls to specify the desired section
+        document.querySelector(`#${targetAccordion.getAttribute('aria-controls')}`).setAttribute('hidden', '');
+    }
+
+}
 
 
 //
 // Inits & Event Listeners
 //
 
-document.addEventListener('click', function(event){
-
-    let target = event.target;
-    
-    if (target.classList.contains('accordion-toggle')){
-
-        let accordion = target.closest('.accordion');
-        let allowMultiple = accordion.hasAttribute('data-allow-multiple');
-  
-        // Check if the current toggle is expanded.
-        let isExpanded = target.getAttribute('aria-expanded') == 'true';
-        let active = accordion.querySelector('[aria-expanded="true"]');
-  
-        // Close the open accordion if not active
-        if (!allowMultiple && active && active !== target) {
-          // Reset the active toggle
-          active.setAttribute('aria-expanded', 'false');
-          // Hide the accordion panel, using aria-controls to specify the desired section
-          document.querySelector(`#${active.getAttribute('aria-controls')}`).setAttribute('hidden', '');
-        }
-  
-        if (!isExpanded) {
-          // Set the expanded state on the triggering element
-          target.setAttribute('aria-expanded', 'true');
-          // Hide the accordion sections, using aria-controls to specify the desired section
-          document.querySelector(`#${target.getAttribute('aria-controls')}`).removeAttribute('hidden');
-        }
-  
-        else if (isExpanded) {
-          // Set the expanded state on the triggering element
-          target.setAttribute('aria-expanded', 'false');
-          // Hide the accordion sections, using aria-controls to specify the desired section
-          document.querySelector(`#${target.getAttribute('aria-controls')}`).setAttribute('hidden', '');
-        }
-  
-        // Enable collapse all button
-        if (accordion.querySelector('[aria-expanded="true"]') && accordion.closest('#browse')){
-          document.querySelector('#browse-collapse').disabled = false;
-        }
-  
-        event.preventDefault();
-        
-    }
-
-})
+document.addEventListener('click', accordionClickHandler);
