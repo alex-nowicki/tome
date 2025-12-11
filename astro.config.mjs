7aa9693c1e4e1897891bd151db346322e5856fde
Alex Nowicki <al_nowicki@hotmail.com>
2025-12-11 15:57:32 -0400

    Set output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
 	integrations: [react(), markdoc(), keystatic()],
 	trailingSlash: 'always',
 	adapter: netlify({
